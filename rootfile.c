993866299dcccb20f2f56af355da5aba608da836
DhruvinK-06 <72988991+DhruvinK-06@users.noreply.github.com>
2020-10-25 13:02:10 +0530

    Update rootfile.c

--- rootfile.c
+++ rootfile.c
@@ -1,201 +1,195 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node
+typedef struct ne
 {
-	int data, height; //h is height of tree
-	struct Node *left, *right, *prnt;  // left child, right child and parent node
-}Node;
+  int data, height; //h is height of tree
+  struct Node *left, *right, *prnt;  // left child, right child and parent node
+}  Node;
+struct node
+{
+  int data;
+  struct node *left, *right;
+}
 Node *rtAVL, *rtBST;         // root of AVL and BST
 
-Node* createNode(int data)
+struct node* createNode(int data)
 {
-    struct node* node = (struct node*)malloc(sizeof(struct node));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
+  struct node* node = (struct node*)malloc(sizeof(struct node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
 }
 
-void insert(Node **rt,int info)
+void insert(Node *rt,int info)
 {
-    struct node* new=createNode(info);
+  struct node* new=createNode(info);
 
-    if (*rt==NULL)
-        *rt=new;
+  if (*rt==NULL)
+    *rt=new;
     else
     {
-        Node* temp=*rt;
-        while (temp!=NULL)
+      Node* temp=*rt;
+      while (temp!=NULL)
+      {
+        if (new->data<temp->data)
         {
-            if (new->data<temp->data)
-            {
-                if (temp->left==NULL)
-                {
-                    temp->left=new;
-                    temp=NULL;
-                }
-                else
-                    temp=temp->left;
-            }
-            else if (new->data>temp->data)
-            {
-                if (temp->right==NULL)
-                {
-                    temp->right=new;
-                    temp=NULL;
-                }
-                else
-                    temp=temp->right;
-            }
-            else
-            {
-                printf("\nDATA ALREADY EXISTS!!!");
-                break;
-            }
+          if (temp->left==NULL)
+          {
+            temp->left=new;
+            temp=NULL;
+          }
+          else
+            temp=temp->left;
+          }
+          else if (new->data>temp->data)
+        {
+          if (temp->right==NULL)
+          {
+            temp->right=new;
+            temp=NULL;
+          }
+          else
+            temp=temp->right;
+          }
+          else
+            break;
         }
+      }
     }
-}
 
-void search(Node** rt,int dat)
-{
+    void search(Node** rt,int dat)
+  {
     if (*rt==NULL)
     {
-        printf("THERE'S NO DATA IN THE TREE\n");
-        return;
+      printf("THERE'S NO DATA IN THE TREE\n");
+      return;
     }
 
     Node* temp=*rt;
     while (temp!=NULL)
     {
-        if (new->data<temp->data)
+      if (new->data<temp->data)
+      {
+        if (temp->left==NULL)
         {
-            if (temp->left==NULL)
-            {
-                printf("\nDATA DOES NOT EXIST\n");
-                return;
-            }
-            else
-                temp=temp->left;
-        }
-        else if (new->data>temp->data)
-        {
-            if (temp->right==NULL)
-            {
-                printf("\nDATA DOES NOT EXIST\n");
-                return;
-            }
-            else
-                temp=temp->right;
+          printf("\nDATA DOES NOT EXIST\n");
+          return;
         }
         else
-        {
-            printf("\nData Found in the tree ");
-            return;
+          temp=temp->left;
         }
+        else if (new->data>temp->data)
+      {
+        if (temp->right==NULL)
+          return;
+        else
+          temp=temp->right;
+        }
+        else
+	  return;
 
+      }
     }
-}
 
-Node* Succ(Node* curr)
-{
+    struct node* Succ(Node* curr)
+  {
     Node *s;
     s=curr->right;
     if (s!=NULL)
     {
-        while (s->left!=NULL)
-            s=s->left;
+      while (s->left!=NULL)
+        s=s->left;
+      }
+      return s;
     }
-    return s;
-}
 
-void del_BST(Node **rt,int dat)
-{
+    void del_BST(Node **rt,int dat)
+  {
     Node *current, *parent=NULL;
 
     current = *rt;
 
     if (rt==NULL)
     {
-        printf("\nNO DATA IN DIRECTORY");
-        return;
+      printf("\nNO DATA IN DIRECTORY");
+      return;
     }
 
     while (current!=NULL && ( dat!=current->data) )
     {
-        parent = current;
-        if (dat<current->data)
-            current=current->left;
+      parent = current;
+      if (dat<current->data)
+        current=current->left;
         else
-            current=current->right;
-    }
+          current=current->right;
+        }
 
-    if (current==NULL)
-        return;
+        if (current==NULL)
+          return;
 
-    if (current->left==NULL && current->right==NULL)
-    {
-        if (current!=*rt)
-        {
-            if (parent->left==current)
+          if (current->left==NULL && current->right==NULL)
+          {
+            if (current!=*rt)
+            {
+              if (parent->left==current)
                 parent->left=NULL;
-            else
-                parent->right=NULL;
-        }
-        else
-            *rt = NULL;
-        free (current);
-    }
+                else
+                  parent->right=NULL;
+                }
+                else
+                  *rt = NULL;
+                  free (current);
+                }
 
-    else if (current->left && current->right)
-    {
-        Node* suc = Succ(current);
-        int tem;
-        tem= suc->data;
-        delete(rt, tem);
-        current->data=tem;
-    }
-    else
-    {
-        Node* child = (current->left)?current->left:current->right;
+                else if (current->left && current->right)
+              {
+                Node* suc = Succ(current);
+                int tem;
+                tem= suc->data;
+                delete(rt, tem);
+                current->data=tem;
+              }
+              else
+              {
+                Node* child = (current->left)?current->left:current->right;
 
-        if (current!=root)
-        {
-            if (current==parent->left)
-                parent->left=child;
-            else
-                parent->right=child;
-        }
-        else
-            *rt=child;
-        free(current);
-    }
-}
+                if (current!=root)
+                {
+                  if (current==parent->left)
+                    parent->left=child;
+                    else
+                      parent->right=child;
+                    }
+                    else
+                      *rt=child;
+                      free(current);
+                    }
+                  }
 
-void InorderDisplay(Node* rt)
-{
-    if (rt==NULL)
-        return;
-    AscendingDisplay(rt->left);
-    printf("%d",rt->data);
-    AscendingDisplay(rt->right);
-}
-
-
+                  void InorderDisplay(Node* rt)
+                {
+                  if (rt==NULL)
+                    return;
+                    AscendingDisplay(rt->left);
+                    printf("%d",rt->data);
+                    AscendingDisplay(rt->right);
+                  }
 
 
 
-void del_AVL(Node *x);    // Del of AVL node
-void kink(Node *x);    // Resolving issue with the problematic grand child in AVL
-void Balance(Node *x); // for checking balance factor
-void lrot(Node *x);    // left rotate node in AVL
-void rrot(Node *x);    // right rotate
-void init(Node *x)     // Initializing values of new node
-{
-	x=(Node*)malloc(sizeof(Node));
-	x->p=NULL;  x->l=NULL;  x->r=NULL;
-	x->k=-1; x->h=0;
-}
-int main()
-{
-	init(ra); init(rb);
-}
+                  void del_AVL(Node *x);    // Del of AVL node
+                  void kink(Node *x);    // Resolving issue with the problematic grand child in AVL
+                  void Balance(Node *x); // for checking balance factor
+                  void lrot(Node *x);    // left rotate node in AVL
+                  void rrot(Node *x);    // right rotate
+                  void init(Node *x)     // Initializing values of new node
+                {
+                  x=(Node*)malloc(sizeof(Node));
+                  x->p=NULL;  x->l=NULL;  x->r=NULL;
+                  x->k=-1; x->h=0;
+                }
+                int main()
+              {
+                init(rtAVL); init(rb);
+              }
